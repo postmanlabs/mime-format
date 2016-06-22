02a3383923fc2986b9344a9c8039bed13cb43438
Shamasis Bhattacharya <mail@shamasis.net>
2016-06-22 18:55:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,14 +2,23 @@ var /**
      * @private
      * @type {Object}
      */
-    db = require('./db.json');
+    db = require('./db.json'),
+    /**
+     * @private
+     * @const
+     * @type {String}
+     */
+    SEP = '/';
 
 module.exports = {
     /**
-     * @param {String|Number} code
-     * @returns {Object} - {name:String, detail:String}
+     * @param {String} mime - contentType header value
+     * @returns {String} - 'text', 'application', 'image', 'video', 'audio' or false
      */
-    lookup: function httpCodeLookup (code) {
-        return db[code] || {};
+    lookup: function mimeFormatLookup (mime) {
+        return mime && (mime = String(mime)) && (mime = mime.replace(/^\s*?([^;\s]+).*$/g, '$1')) && db[mime] || {
+            type: (mime = mime.split(SEP)) && mime[0],
+            format: 'raw'
+        };
     }
 };
