29ddd9290afe4e46380fbef4f9808222834d261c
Shamasis Bhattacharya <mail@shamasis.net>
2016-08-03 18:05:46 +0530

    Added a `guess` function to guess format when lookup failed

--- index.js
+++ index.js
@@ -12,13 +12,32 @@ var /**
 
 module.exports = {
     /**
+     * Attempts to guess the format by analysing mime type and not a db lookup
+     *
      * @param {String} mime - contentType header value
-     * @returns {String} - 'text', 'application', 'image', 'video', 'audio' or false
+     *
+     * @returns {Object} 
+     */
+    guess: function (mime) {
+        var match = mime.match(/(jsonp|json|[a-z]{1,2}ml|script)/),
+            format = match && match[1];
+
+        return format ? {
+            type: 'text',
+            format: format,
+            guessed: true
+        } : { // default is `raw` for all unknown stuff
+            type: (mime = mime.split(SEP)) && mime[0],
+            format: 'raw',
+            guessed: true
+        };
+    },
+    /**
+     * @param {String} mime - contentType header value
+     * @returns {Object}
      */
     lookup: function mimeFormatLookup (mime) {
-        return mime && (mime = String(mime)) && (mime = mime.replace(/^\s*?([^;\s]+).*$/g, '$1')) && db[mime] || {
-            type: (mime = mime.split(SEP)) && mime[0],
-            format: 'raw'
-        };
+        return mime && (mime = String(mime)) && (mime = mime.replace(/^\s*?([^;\s]+).*$/g, '$1')) && db[mime] || 
+            module.exports.guess(mime);
     }
 };
