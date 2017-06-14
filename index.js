a9e2ca50433283dbe98f29e0716705c36dd832cd
Prashant Agrawal <prashant@getpostman.com>
2017-06-14 17:37:55 +0530

    Add jsdoc documentation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var /**
      * @private
+     * @const
      * @type {Object}
      */
     db = require('./db.json'),
@@ -7,36 +8,42 @@ var /**
      * @private
      * @const
      * @type {String}
+     * @default
      */
     SEP = '/',
     /**
      * @private
-     * @const
-     * @type {string}
+     * @cons
+     * @type {String}
+     * @default
      */
     E = '',
     /**
      * @private
-     * @const
-     * @type {string}
+     * @constant
+     * @type {String}
+     * @default
      */
     TEXT = 'text',
     /**
      * @private
-     * @const
-     * @type {string}
+     * @constant
+     * @type {String}
+     * @default
      */
     RAW = 'raw',
     /**
      * @private
-     * @const
-     * @type {string}
+     * @constant
+     * @type {String}
+     * @default
      */
     UNKNOWN = 'unknown',
     /**
      * @private
-     * @const
-     * @type {string}
+     * @constant
+     * @type {String}
+     * @default
      */
     PLAIN = 'plain',
 
@@ -44,44 +51,53 @@ var /**
      * All the content type base types that mostly mean what they are meant to do!
      *
      * @private
-     * @const
+     * @constant
      * @type {RegExp}
+     * @default
      */
     AUDIO_VIDEO_IMAGE_TEXT = /(audio|video|image|text)/,
     /**
      * A blanket check for commonly used keywords that are associated with text content type
      *
      * @private
-     * @const
+     * @constant
      * @type {RegExp}
+     * @default
      */
     JSON_XML_SCRIPT_SIBLINGS = /(jsonp|json|xml|html|yaml|vml|webml|script)/,
     /**
      * Same check as the sure-shot bases, except that these must be sub-types
      *
      * @private
-     * @const
+     * @constant
      * @type {RegExp}
+     * @default
      */
     AUDIO_VIDEO_IMAGE_TEXT_SUBTYPE = /\/[^\/]*(audio|video|image|text)/,
     /**
      * The content type bases that are not well defined or ambiguous to classify
      *
      * @private
-     * @const
+     * @constant
      * @type {RegExp}
+     * @default
      */
     APPLICATION_MESSAGE_MULTIPART = /(application|message|multipart)/;
+
+/**
+ * Simple module to lookup the base format of HTTP response bodies from the content-type header
+ * @module mime-format
+ */
 
 module.exports = {
     /**
      * Attempts to guess the format by analysing mime type and not a db lookup
-     *
+     * @private
      * @param {String} mime - contentType header value
-     *
      * @returns {Object}
      */
     guess: function (mime) {
+
         var info = {
                 type: UNKNOWN,
                 format: RAW,
@@ -137,8 +153,39 @@ module.exports = {
     },
 
     /**
-     * @param {String} mime - contentType header value
+     * Finds the mime-format of the provided Content-Type
+     * @param {String} mime - Content-Type for which you want to find the mime format. e.g <b><i>application/xml</i></b>.
      * @returns {Object}
+     * @example <caption>Basic usage</caption>
+     * mimeFormat.lookup('application/xml');
+     *
+     * // Output
+     * // {
+     * //   "type": "text",
+     * //   "format": "xml"
+     * // }
+     *
+     * @example <caption>Content-Type with charset</caption>
+     * mimeFormat.lookup('application/xml; charset=gBk');
+     *
+     * // Output
+     * // {
+     * //   "type": "text",
+     * //   "format": "xml",
+     * //   "charset": "gBk"
+     * // }
+     *
+     * @example <caption>Unknown Content-Type</caption>
+     * mimeFormat.lookup('random/abc');
+     *
+     * // Output
+     * // {
+     * //   "type": "unknown",
+     * //   "format": "raw",
+     * //   "guessed": true,
+     * //   "orphan": true
+     * // }
+     *
      */
     lookup: function mimeFormatLookup (mime) {
         var charset = require('charset')(mime);
